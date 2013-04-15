dd42cfc88f661efc942e58d24fe63e18c22fe69e
luozhixiang <luozhixiang82@gmail.com>
2013-04-15 18:38:23 +0800

    #make the menu to one level

--- js/MainScreen.js
+++ js/MainScreen.js
@@ -11,11 +11,35 @@
             var $e = view.$el;
             view.$container = $e.find(".MainScreen-main");
             brite.display("ZoomCluster",view.$container);
-            	//$('.dropdown-toggle').dropdown("toggle");
+            $e.find("li.D3JS").hide();
         },
         
         events:{
-        	"click; .MainScreen-header .navbar-inner .nav li.menu":function(event){
+	        	"btap; .nav li.nav-menu":function(e){
+	    		var view = this;
+	    		var $e = view.$el;
+	    		var $li = $(e.currentTarget);
+	    		$e.find("li.nav-menu").removeClass("active");
+	    		$li.addClass("active");
+	    		var menu = $li.attr("data-nav");
+	    		
+	    		if(menu == "D3JS"){
+	    			$e.find("li.EaselJS").hide();
+	    		  	$e.find("li.D3JS").show();
+	        		brite.display("ContactCluster",view.$container);
+	        		$e.find(".nav > li").removeClass("active");
+	        		$e.find(".nav > li.menu[data-nav='ContactCluster']").addClass("active");
+	    		}else if(menu == "EaselJS"){
+	    			$e.find("li.EaselJS").show();
+	    		  	$e.find("li.D3JS").hide();
+	        		brite.display("ZoomCluster",view.$container);
+	        		$e.find(".nav > li").removeClass("active");
+	        		$e.find(".nav > li.menu[data-nav='ZoomCluster']").addClass("active");
+	    		}
+	    		$li.closest(".dropdown").find(".dropDownTitle").html(menu);
+	    	},
+	    	
+        	"btap; .MainScreen-header .navbar-inner .nav > li.menu":function(event){
         		var view = this;
         		var $this = $(event.currentTarget);
         		var chart = $this.attr("data-nav");
